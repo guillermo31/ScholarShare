c0a9138016c5b79c59917ab5c541efc8f3341642
Guillermo Ramos <memo33101@gmail.com>
2019-04-10 19:38:58 -0600

    Started add button onclick method and updated scholarship class

--- app/src/main/java/com/ctecgang/scholarshare/Scholarship.java
+++ app/src/main/java/com/ctecgang/scholarshare/Scholarship.java
@@ -1,18 +1,27 @@
 package com.ctecgang.scholarshare;
 
 public class Scholarship {
-    private String ACT;
-    private String Gender;
-    private String GPA;
-    private String Ethnicity;
-    private String State;
+    private String act;
+    private String gender;
+    private String gpa;
+    private String ethnicity;
+    private String state;
     protected Scholarship()
     {
-        this.ACT = "";
-        this.Gender = "";
-        this.GPA = "";
-        this.Ethnicity = "";
-        this.State = "";
+        this.act = "";
+        this.gender = "";
+        this.gpa = "";
+        this.ethnicity = "";
+        this.state = "";
+    }
+
+    protected Scholarship(String act, String gender, String gpa, String ethnicity, String state)
+    {
+        this.act = act;
+        this.gender = gender;
+        this.gpa = gpa;
+        this.ethnicity = ethnicity;
+        this.state = state;
     }
 
 }
