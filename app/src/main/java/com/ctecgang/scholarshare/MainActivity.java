9b998cffa32f18eb63c1f13d8c37480b86c0b289
Ryan <jlkfop@gmail.com>
2019-04-11 12:11:47 -0600

    I moved things a little bit

--- app/src/main/java/com/ctecgang/scholarshare/MainActivity.java
+++ app/src/main/java/com/ctecgang/scholarshare/MainActivity.java
@@ -1,10 +1,10 @@
 package com.ctecgang.scholarshare;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Button;
-import android.view.View;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.Button;
+        import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity
