8e92247faf59f64c5609e4932f314066f0304858
rphi8796 <jlkfop@gmail.com>
2019-04-11 13:28:13 -0600

    Created a listener for the button in activity_user_input.xml. It switches to display.

--- app/src/main/java/com/ctecgang/scholarshare/activity_user_input.java
+++ app/src/main/java/com/ctecgang/scholarshare/activity_user_input.java
@@ -1,13 +1,35 @@
 package com.ctecgang.scholarshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class activity_user_input extends AppCompatActivity {
+    private Button scholarShipDisplayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input);
+        scholarShipDisplayButton = (Button) findViewById(R.id.SearchButton);
+        scholarShipDisplayButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v)
+            {
+                openDisplayScreen();
+            }
+
+
+        });
     }
+
+
+public void openDisplayScreen()
+{
+    Intent intent = new Intent(this, Display.class);
+    startActivity(intent);
+}
 }
