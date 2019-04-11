d9b7dac4877b6b836933a75cad7ab360141f276f
Guillermo Ramos <memo33101@gmail.com>
2019-04-11 13:13:10 -0600

    Updated user activity user input to have read from firebase functionality

--- app/src/main/java/com/ctecgang/scholarshare/activity_user_input.java
+++ app/src/main/java/com/ctecgang/scholarshare/activity_user_input.java
@@ -1,13 +1,46 @@
 package com.ctecgang.scholarshare;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class activity_user_input extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private ArrayList<Scholarship> scholarships = new ArrayList<Scholarship>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input);
+
+
+    }
+
+    protected void readScholarships()
+    {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
