5dd1eef7e5b3f248668f7fc5cc227a9703ac56eb
Ryan <jlkfop@gmail.com>
2019-04-11 14:37:54 -0600

    Finished linking GUi with firebase

--- app/src/main/java/com/ctecgang/scholarshare/add.java
+++ app/src/main/java/com/ctecgang/scholarshare/add.java
@@ -7,11 +7,13 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.*;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+
 
 
 public class add extends AppCompatActivity implements AdapterView.OnItemSelectedListener
@@ -27,35 +29,38 @@ public class add extends AppCompatActivity implements AdapterView.OnItemSelected
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-        Spinner actSpinner = findViewById(R.id.ACTspinner);
+        final Spinner actSpinner = findViewById(R.id.ACTspinner);
         ArrayAdapter<CharSequence> actAdapter = ArrayAdapter.createFromResource(this, R.array.ACTScores, android.R.layout.simple_spinner_item);
         actAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         actSpinner.setAdapter(actAdapter);
         actSpinner.setOnItemSelectedListener(this);
 
-        Spinner gpaSpinner = findViewById(R.id.GPASpinner);
+        final Spinner gpaSpinner = findViewById(R.id.GPASpinner);
         ArrayAdapter<CharSequence> gpaAdapter = ArrayAdapter.createFromResource(this, R.array.GPAList, android.R.layout.simple_spinner_item);
         gpaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         gpaSpinner.setAdapter(gpaAdapter);
         gpaSpinner.setOnItemSelectedListener(this);
 
-        Spinner ethnicitySpinner = findViewById(R.id.GenderSpinner);
-        ArrayAdapter<CharSequence> ethnicityAdapter = ArrayAdapter.createFromResource(this, R.array.genderList, android.R.layout.simple_spinner_item);
+        final Spinner ethnicitySpinner = findViewById(R.id.GenderSpinner);
+        final ArrayAdapter<CharSequence> ethnicityAdapter = ArrayAdapter.createFromResource(this, R.array.genderList, android.R.layout.simple_spinner_item);
         ethnicityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         ethnicitySpinner.setAdapter(ethnicityAdapter);
         ethnicitySpinner.setOnItemSelectedListener(this);
 
-        Spinner genderSpinner = findViewById(R.id.EthnicitySpinnner);
+        final Spinner genderSpinner = findViewById(R.id.EthnicitySpinnner);
         ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this, R.array.ethnicityList, android.R.layout.simple_spinner_item);
         genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genderSpinner.setAdapter(genderAdapter);
         genderSpinner.setOnItemSelectedListener(this);
 
-        Spinner stateSpinner = findViewById(R.id.StateSpinner);
+        final Spinner stateSpinner = findViewById(R.id.StateSpinner);
         ArrayAdapter<CharSequence> stateAdapter = ArrayAdapter.createFromResource(this, R.array.stateList, android.R.layout.simple_spinner_item);
         stateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         stateSpinner.setAdapter(stateAdapter);
         stateSpinner.setOnItemSelectedListener(this);
+
+        final EditText name = findViewById(R.id.editText2);
+
 
 
 
@@ -68,7 +73,15 @@ public class add extends AppCompatActivity implements AdapterView.OnItemSelected
             @Override
             public void onClick(View v)
             {
-                newScholarship = new Scholarship();
+                String actScore = actSpinner.getSelectedItem().toString();
+                String gpa = gpaSpinner.getSelectedItem().toString();
+                String ethnicity = ethnicitySpinner.getSelectedItem().toString();
+                String gender = genderSpinner.getSelectedItem().toString();
+                String state = stateSpinner.getSelectedItem().toString();
+                String nameField = name.getText().toString();
+
+                newScholarship = new Scholarship(nameField, actScore, gender, gpa, ethnicity, state);
+
                 mDatabase.child("scholarships").child(newScholarship.getName()).setValue(newScholarship);
 
             }
@@ -79,9 +92,7 @@ public class add extends AppCompatActivity implements AdapterView.OnItemSelected
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
     {
-        String actScore = parent.getItemAtPosition(position).toString();
-        String gpaValue = parent.getItemAtPosition(position).toString();
-
+        String value = parent.getItemAtPosition(position).toString();
     }
 
     @Override
