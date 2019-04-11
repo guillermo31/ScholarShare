d5b2e92f37f138ac197fecc0155dfc1bfb6fa821
Ryan <jlkfop@gmail.com>
2019-04-11 09:43:30 -0600

    I figured out how to add values to a Spinner, now I just need to add all the values

--- app/src/main/java/com/ctecgang/scholarshare/add.java
+++ app/src/main/java/com/ctecgang/scholarshare/add.java
@@ -2,13 +2,38 @@ package com.ctecgang.scholarshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
-public class add extends AppCompatActivity
+public class add extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        Spinner actSpinner = findViewById(R.id.ACTSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.ACTScores,
+        android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        actSpinner.setAdapter(adapter);
+        actSpinner.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        String text = parent.getItemAtPosition(position).toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+
     }
 }
