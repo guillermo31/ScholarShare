64b81c571c44d87159a61933411e8064585158b9
Ryan <jlkfop@gmail.com>
2019-04-11 14:42:16 -0600

    Moved code to correct file

--- app/src/main/java/com/ctecgang/scholarshare/activity_user_input.java
+++ app/src/main/java/com/ctecgang/scholarshare/activity_user_input.java
@@ -14,8 +14,7 @@ import java.util.ArrayList;
 
 public class activity_user_input extends AppCompatActivity {
 
-    private DatabaseReference mDatabase;
-    private ArrayList<Scholarship> scholarships = new ArrayList<Scholarship>();
+
 
 
     @Override
@@ -26,40 +25,5 @@ public class activity_user_input extends AppCompatActivity {
 
     }
 
-    public interface DataStatus
-    {
-        void dataIsLoaded(ArrayList<Scholarship> scholarships, ArrayList<String> keys);
-        void dataIsInserted();
-        void dataIsUpdated();
-        void dataIsDeleted();
 
-    }
-
-
-    protected void readScholarships(final DataStatus dataStatus)
-    {
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-
-        mDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                ArrayList<String> keys = new ArrayList<String>();
-
-                for(DataSnapshot keyNode : dataSnapshot.getChildren())
-                {
-                    keys.add(keyNode.getKey());
-                    Scholarship currentScholarship = keyNode.getValue(Scholarship.class);
-                    scholarships.add(currentScholarship);
-                }
-                dataStatus.dataIsLoaded(scholarships, keys);
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-    }
 }
